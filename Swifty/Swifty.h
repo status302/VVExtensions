78014e3829bafdfda9ab3b9a251910d406201274
qiuncheng <qiuncheng@gmail.com>
2018-04-27 20:18:28 +0800

    initial commit.

---
+++ Swifty/Swifty.h
@@ -0,0 +1,19 @@
+//
+//  Swifty.h
+//  Swifty
+//
+//  Created by vsccw on 2018/4/27.
+//  Copyright © 2018年 vsccw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifty.
+FOUNDATION_EXPORT double SwiftyVersionNumber;
+
+//! Project version string for Swifty.
+FOUNDATION_EXPORT const unsigned char SwiftyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty/PublicHeader.h>
+
+
