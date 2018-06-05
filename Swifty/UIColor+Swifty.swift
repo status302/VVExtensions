e69dd56e211561d693dbff764a03532156473511
Qiuncheng <qiuncheng@gmail.com>
2018-06-06 01:05:57 +0800

    Update add UIImage+Swifty and UIColor+Swifty

---
+++ Swifty/UIColor+Swifty.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Swifty.swift
+//  Swifty
+//
+//  Created by 程庆春 on 2018/6/6.
+//  Copyright © 2018年 vsccw. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+  
+}
