dcaa87762c4050986c4034eb44f0df55cd9beb85
Qiuncheng <qiuncheng@gmail.com>
2018-06-08 00:31:18 +0800

    Update to add UIColor(rgb:) init.

--- Swifty/UIColor+Swifty.swift
+++ Swifty/UIColor+Swifty.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 public extension UIColor {
-  
+  public convenience init(rgb: Int) {
+    self.init(rgb: rgb, alpha: 1.0)
+  }
+
+  public convenience init(rgb: Int, alpha: CGFloat) {
+    let red = CGFloat((rgb >> 16) & 0xFF)
+    let green = CGFloat((rgb >> 8) & 0xFF)
+    let blue = CGFloat(rgb & 0xFF)
+    self.init(red: red, green: green, blue: blue, alpha: alpha)
+  }
 }
