6fa270660fcea4d7ba9d1c377360f39fa763b018
Qiuncheng <qiuncheng@gmail.com>
2018-06-13 00:43:32 +0800

    Update add UIColor extension for transition to color with ratio

--- Swifty/UIColor+Swifty.swift
+++ Swifty/UIColor+Swifty.swift
@@ -20,3 +20,53 @@ public extension UIColor {
     self.init(red: red, green: green, blue: blue, alpha: alpha)
   }
 }
+
+public extension UIColor {
+
+  struct Components {
+    let red: CGFloat
+    let green: CGFloat
+    let blue: CGFloat
+    let alpha: CGFloat
+  }
+
+  public func transitionToColor(_ color: UIColor, ratio: CGFloat) -> UIColor {
+    let offset = calculateColorOffset(withColor: color)
+    let current = rgbs
+    let red = current.red + ratio * offset.red
+    let green = current.green + ratio * offset.green
+    let blue = current.blue + ratio * offset.blue
+    let alpha = current.alpha + ratio * offset.alpha
+    return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+  }
+
+  private var rgbs: Components {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    if getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+      return Components(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    else if let components = cgColor.components, components.count > 3 {
+      let red = components[0]
+      let green = components[1]
+      let blue = components[2]
+      let alpha = components[3]
+      return Components(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    else {
+      return Components(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+  }
+
+  private func calculateColorOffset(withColor color: UIColor) -> Components {
+    let begin = rgbs
+    let end = color.rgbs
+    return Components(red: end.red - begin.red,
+                      green: end.green - begin.green,
+                      blue: end.blue - begin.blue,
+                      alpha: end.alpha - begin.alpha)
+  }
+}
+
