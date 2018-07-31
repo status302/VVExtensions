6896bda8054529e2528a91bc7bd5502c504637e3
Qiuncheng <qiuncheng@gmail.com>
2018-08-01 00:38:42 +0800

    Update to add UICollectionView+Swifty

---
+++ VVExtensions/UICollectionView+Swifty.swift
@@ -0,0 +1,18 @@
+//
+//  UICollectionView.swift
+//  VVExtensions
+//
+//  Created by 程庆春 on 2018/7/28.
+//  Copyright © 2018 vsccw. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionView {
+  public func reloadDataWithoutAnimation() {
+    CATransaction.begin()
+    CATransaction.disableActions()
+    reloadData()
+    CATransaction.commit()
+  }
+}
